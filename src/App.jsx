7609440aa7cd8be012f1280cf677b0f5a1deb5f0
kunal khandelwal <kunalkhandelwal6999@gmail.com>
2024-06-25 21:56:05 +0530

    added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-function App() {
+import { useParams } from "react-router-dom";
 
+const App = () => {
+  const {id} = useParams();
   return (
-    <div className='bg-red-500 p-2 text-center rounded-lg'>
-      Custom Resume Builder
+    <div className="bg-red-500 p-2 text-center rounded-lg">
+      Custom Resume Builder {id}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import Custom from "./components/Custom/Custom.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="custom" element={<Custom />} />
+      <Route path="/:id" element={<App />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
